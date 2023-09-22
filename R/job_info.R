b69bbf8686bc750e0a0b1f603d792b50b2a6cb02
Nick-Eagles <nick.eagles@libd.org>
2023-09-22 15:50:14 -0400

    Clean up output from squeue, don't parse REASON column for now, since it's tricky and not usually interesting; remove the 'memory' parameter, since there's not a good reason a user would want it to be FALSE

--- R/job_info.R
+++ R/job_info.R
@@ -4,8 +4,6 @@
 #' query. Set NULL to return info about all users' jobs.
 #' @param partition A `character(1)` vector specifying the partition of the jobs
 #' to query. Set NULL to return info about jobs of all paritions.
-#' @param memory A `logical(1)` vector determining whether to return memory
-#' usage information (only possible/ applicable for your jobs)
 #'
 #' @return A tibble with job information about currently running jobs.
 #' @export
@@ -16,11 +14,11 @@
 #'
 #' [TODO!]
 
-job_info = function(user = Sys.getenv('USER'), partition = 'shared', memory = TRUE) {
+job_info = function(user = Sys.getenv('USER'), partition = 'shared') {
     #   Read several key fields from squeue and parse into a tibble
     job_df = read.csv(
             text = system(
-                'squeue --format="%u,%A,%K,%j,%P,%C,%m,%t,%r"', intern = TRUE
+                'squeue --format="%u,%A,%K,%j,%P,%C,%m,%t"', intern = TRUE
             )
         ) |>
         as_tibble()
@@ -30,33 +28,46 @@ job_info = function(user = Sys.getenv('USER'), partition = 'shared', memory = TR
     if (!is.null(user)) job_df = job_df |> filter(USER == user)
     if (!is.null(partition)) job_df = job_df |> filter(PARTITION == partition)
 
-    #   Only interested in running jobs
-    job_df = job_df |> filter(ST == 'R')
+    #   Do some initial data cleaning
+    job_df = job_df |>
+        #   Only interested in running jobs
+        filter(ST == 'R') |>
+        mutate(
+            #   Fix NAs that are actually of character type
+            ARRAY_TASK_ID = ifelse(ARRAY_TASK_ID == "N/A", NA, ARRAY_TASK_ID),
+            #   Some character columns should be factors
+            PARTITION = as.factor(PARTITION),
+            STATUS = as.factor(ST)
+        ) |>
+        rename(
+            JOB_ID = JOBID,
+            REQUESTED_MEM = MIN_MEMORY
+        ) |>
+        select(-ST)
+    colnames(job_df) = tolower(colnames(job_df))
 
-    if (memory) {
-        #   Form a list of tibbles, containing job ID and maximum physical and
-        #   virtual memory used
-        mem_df_list = lapply(
-            #   Only have permission to check memory of this user's jobs
-            job_df |> filter(USER == Sys.getenv('USER')) |> pull(JOBID),
-            function(x) {
-                command = sprintf(
-                    'sstat -P -j %s --format="JobID,MaxRSS,MaxVMSize"', x
-                )
-                mem_df = read.csv(
-                        text = system(command, intern = TRUE), sep = "|"
-                    ) |>
-                    as_tibble()
-            }
-        )
+    #   Form a list of tibbles, containing job ID and maximum physical and
+    #   virtual memory used
+    mem_df_list = lapply(
+        #   Only have permission to check memory of this user's jobs
+        job_df |> filter(USER == Sys.getenv('USER')) |> pull(job_id),
+        function(x) {
+            command = sprintf(
+                'sstat -P -j %s --format="JobID,MaxRSS,MaxVMSize"', x
+            )
+            mem_df = read.csv(
+                    text = system(command, intern = TRUE), sep = "|"
+                ) |>
+                as_tibble()
+        }
+    )
 
-        #   Combine list of tibbles and merge with existing tibble
-        job_df = do.call(rbind, mem_df_list) |>
-            rename(
-                JOBID = JobID,
-                MAX_RSS = MaxRSS,
-                MAX_VMEM = MaxVMSize
-            ) |>
-            right_join(job_df)
-    }
+    #   Combine list of tibbles and merge with existing tibble
+    job_df = do.call(rbind, mem_df_list) |>
+        rename(
+            job_id = JobID,
+            max_rss = MaxRSS,
+            max_vmem = MaxVMSize
+        ) |>
+        right_join(job_df)
 }
