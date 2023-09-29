9c8a564ee0c47e6918273aea42efe9c857d39105
Nick-Eagles <nick.eagles@libd.org>
2023-09-29 16:27:14 -0400

    Better document the testing script

--- dev/interactive_testing/broken_array.R
+++ dev/interactive_testing/broken_array.R
@@ -1,3 +1,6 @@
+#   This script runs as part of an array job, and only specific task IDs are
+#   intended to fail (nonzero exit status)
+
 task_id = Sys.getenv('SLURM_ARRAY_TASK_ID')
 
 bad_ids = c(1, 4, 7:9)
