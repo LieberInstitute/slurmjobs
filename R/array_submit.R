0882d676ba1f7ad3c7481426eefd365b14146709
Nick-Eagles <nick.eagles@libd.org>
2023-10-04 10:38:51 -0400

    Update the header/ documentation for 'array_submit'; add a 'job_id' parameter to optionally use in place of 'task_ids'; add checks for legitimate use of 'task_ids' and 'job_id' arguments

--- R/array_submit.R
+++ R/array_submit.R
@@ -1,13 +1,21 @@
 #' Submit an array job with a specified set of task IDs
 #'
-#' Given a bash script that specifies the SGE option -t (that is, an array job),
-#' this function submits that job using `qsub` for a given list of task
-#' IDs. This function is useful in case some tasks for a given job failed which
-#' you can find using `qstat | grep Eqw` or other options.
+#' Given a bash script that specifies the --array `sbatch` option (that is, an
+#' array job), this function overwrites (temporarily if `restore` = TRUE) the
+#' script in place and resubmits (when `submit` = TRUE) the array with the
+#' specified `task_ids`. Alternatively, the job ID (`job_id`) for the original
+#' run of the array job may be specified, and failed tasks are automatically
+#' inferred. This function is intended to help re-run failed tasks of a large
+#' array job that was previously submitted.
 #'
 #' @param job_bash A `character(1)` vector with the name of a bash script
 #' in the current working directory.
-#' @param task_ids A set of task ids that will get parsed by [parse_task_ids].
+#' @param task_ids A numeric vector specifying which (relative) task IDs to
+#' resubmit (e.g. c(1, 4, 6)), or NULL if 'job_id' is provided.
+#' @param job_id Optional `numeric(1)` or `character(1)` specifying the job ID
+#' for the initial submission of the array job whose path is given by
+#' `job_bash`. The array is assumed to have run with all of its tasks. If
+#' provided, `task_ids` will automatically be inferred by which tasks failed.
 #' @param submit A `logical(1)` vector determining whether to actually submit
 #' the tasks or not using `qsub`.
 #' @param restore A `logical(1)` vector determining whether to restore the
@@ -17,7 +25,6 @@
 #' @export
 #' @author Leonardo Collado-Torres
 #' @author Nicholas J. Eagles
-#' @import purrr glue
 #'
 #' @examples
 #'
@@ -33,15 +40,25 @@
 #'         task_num = 100
 #'     )
 #'
-#'     ## Now we can submit the SGE job for a set of task IDs
+#'     ## Now we can submit the job for a set of task IDs
 #'     array_submit(
 #'         job_bash = paste0(job_name, ".sh"),
-#'         task_ids = "225019-225038:1,225040,225043",
+#'         task_ids = c(1, 6, 8:20, 67),
+#'         submit = FALSE
+#'     )
+#' 
+#'     ## Or specify the ID for the original array submission to automatically
+#'     ## grab those same failed task IDs
+#'     array_submit(
+#'         job_bash = paste0(job_name, ".sh"),
+#'         job_id = 235510,
 #'         submit = FALSE
 #'     )
 #' })
 #'
-array_submit <- function(job_bash, task_ids, submit = FALSE, restore = TRUE) {
+array_submit <- function(
+        job_bash, task_ids = NULL, job_id = NULL, submit = FALSE, restore = TRUE
+    ) {
     ## Check that the script is in the working directory
     if (basename(job_bash) != job_bash) {
         stop(
@@ -51,8 +68,17 @@ array_submit <- function(job_bash, task_ids, submit = FALSE, restore = TRUE) {
         )
     }
 
+    #   Check that at least one of 'task_ids' and 'job_id' is not NULL. If both
+    #   are defined, 'task_ids' is ignored with a warning
+    if (!(is.null(job_id) || is.null(task_ids))) {
+        stop("Either 'job_id' or 'task_ids' must be non-NULL.")
+    }
+    if (!is.null(job_id) & !is.null(task_ids)) {
+        warning("Ignoring 'task_ids' since 'job_id' was not NULL.")
+    }
+
     job_original <- readLines(job_bash)
-    t_line <- grep("^#SBATCH --array=", job_original))
+    t_line <- grep("^#SBATCH --array=", job_original)
     if (length(t_line) != 1) {
         stop("Could not find the line that specifies that this is an array job,\n",
             "that is, the line that starts with: '#SBATCH --array='",
