f39a3df80a4d3b99e28e8daac9ddc07a2a692892
Nick-Eagles <nick.eagles@libd.org>
2023-10-10 13:12:13 -0400

    Actually, provide 'partition_df' as data since it was useful to provide summaries of the partitions, and some info needed to be anonymized (and thus 'partition_df's origin should be documented)

--- R/data.R
+++ R/data.R
@@ -3,3 +3,9 @@
 #' A tibble with 100 rows and 10 columns describing currently running jobs (at
 #' the time of creation)
 "job_info_df"
+
+#' Example output from \code{partition_info(partition = NULL, all_nodes = FALSE)}
+#'
+#' A tibble with 5 rows and 7 columns giving information about memory and CPUs
+#' as available at the time of creation)
+"partition_df"
---
+++ data-raw/partition_df.R
@@ -0,0 +1,7 @@
+partition_df = partition_info(partition = NULL, all_nodes = FALSE) |>
+    #   Randomly choose 5 partitions
+    slice_sample(n = 5) |>
+    #   Remove information about true partition names
+    mutate(partition = paste0('partition_', 1:5))
+
+usethis::use_data(partition_df, overwrite = TRUE)
