e7484695ac56f003e3aa77b91b0fcbda7585e14d
Nick-Eagles <nick.eagles@libd.org>
2023-10-10 12:11:21 -0400

    Document the 'job_info_df' data

---
+++ R/data.R
@@ -0,0 +1,5 @@
+#' Example output from \code{job_info(user = NULL, partition = "shared")}
+#'
+#' A tibble with 100 rows and 10 columns describing currently running jobs (at
+#' the time of creation)
+"job_info_df"
