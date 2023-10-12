a6acd27e79e86997f2f86c5f021845c567ae6a40
Nick-Eagles <nick.eagles@libd.org>
2023-10-12 13:32:38 -0400

    Add a job_loop shell script with some failed task IDs (later to test 'array_submit')

---
+++ dev/interactive_testing/broken_array_loop.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH --mem-per-cpu=2G
+#SBATCH --job-name=broken_array_loop
+#SBATCH -c 1
+#SBATCH -o /dev/null
+#SBATCH -e /dev/null
+#SBATCH --mail-type=ALL
+#SBATCH --array=1-30%20
+
+## Define loops and appropriately subset each variable for the array task ID
+all_letter=(a b c)
+letter=${all_letter[$(( $SLURM_ARRAY_TASK_ID / 10 % 3 ))]}
+
+all_number=(1 2)
+number=${all_number[$(( $SLURM_ARRAY_TASK_ID / 5 % 2 ))]}
+
+all_combo=(one two three four five)
+combo=${all_combo[$(( $SLURM_ARRAY_TASK_ID / 1 % 5 ))]}
+
+## Explicitly pipe script output to a log
+log_path=logs/${letter}_${number}_${combo}_${SLURM_ARRAY_TASK_ID}.log
+
+{
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load the R module
+module load conda_R/4.3
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript -e "options(width = 120); print('${letter}'); print('${number}'); print('${combo}'); sessioninfo::session_info()"
+
+#   Make some tasks fail
+if [[ number -eq 1 ]]; then
+    echo "Failed task"
+    exit 1
+fi
+
+echo "**** Job ends ****"
+date
+
+} > $log_path 2>&1
+
+## This script was made using slurmjobs version 0.99.0
+## available from http://research.libd.org/slurmjobs/
+
