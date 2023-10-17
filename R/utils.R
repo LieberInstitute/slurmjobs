43e337ea11d07cdb20d530a06a4bd204cb005be4
Nick-Eagles <nick.eagles@libd.org>
2023-10-17 14:33:40 -0400

    Add a function for getting unique initials of a character vector. Preparing for using getopt with short flags

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,22 @@ get_list_indexing <- function(this_list, index) {
 
     return(list("divisor" = divisor, "modulus" = modulus))
 }
+
+#   Given a character vector, return an equal-length character vector of
+#   unique one-letter initials
+get_short_flags = function(loops) {
+    #   Try just taking the first letter of the names
+    short_flags = substr(names(loops), 1, 1)
+
+    #   Get unused letters
+    remaining_letters = setdiff(letters, short_flags)
+    if (length(remaining_letters) == 0) {
+        stop("Can't handle more than 26 loops.")
+    }
+
+    #   Overwrite duplicates with unused letters
+    dup_flags = duplicated(short_flags)
+    short_flags[dup_flags] = remaining_letters[1:length(which(dup_flags))]
+
+    return(short_flags)
+}
