df83272ece8e9b9adf1b2d9e026ade9e88e01099
Nick-Eagles <nick.eagles@libd.org>
2023-10-18 16:51:55 -0400

    Add roxygen docs for remaining utils.R functions

--- R/utils.R
+++ R/utils.R
@@ -46,11 +46,35 @@ get_list_indexing <- function(this_list, index) {
     return(list("divisor" = divisor, "modulus" = modulus))
 }
 
-#   Given a character vector 'loops', return an equal-length character vector of
-#   unique one-letter initials
-get_short_flags <- function(loops) {
+#' Find initials for a character vector
+#'
+#' Given a character vector 'vec', return an equal-length character vector of
+#' unique one-letter initials. The first letter of each string will be used,
+#' except when this results in duplicates; unique letters will be arbitrarily
+#' assigned in alphabetical order in the case of duplicates.
+#'
+#' @param vec A character vector, assumed to start with alphabetical characters
+#'
+#' @return A character vector with the same length as \code{vec} giving unique
+#' one-letter initials
+#'
+#' @author Nicholas J. Eagles
+#'
+#' @examples
+#'
+#' #    Simple example where initials are as expected: 'a', 'b', 'c'
+#' fruits = c('apple', 'banana', 'cherry')
+#' initials = get_short_flags(fruits)
+#' print(fruits)
+#' 
+#' #    Example with duplicates with some arbitrary initials: 'c', 'a', 'b', 'd'
+#' fruits = c('coconut', 'cherry', 'banana', 'berry')
+#' initials = get_short_flags(fruits)
+#' print(fruits)
+#'
+get_short_flags <- function(vec) {
     #   Try just taking the first letter of the names
-    short_flags <- substr(loops, 1, 1)
+    short_flags <- substr(vec, 1, 1)
 
     #   Get unused letters
     remaining_letters <- setdiff(letters, short_flags)
@@ -65,8 +89,23 @@ get_short_flags <- function(loops) {
     return(short_flags)
 }
 
-#   Given a character vector 'vec', return a character(1) representing the line
-#   of code used to generate 'vec'
+#' Get the segment of code used to construct a given character vector
+#'
+#' Given a character vector 'vec', return a character(1) representing the line
+#' of code used to generate 'vec'
+#'
+#' @param vec A character vector
+#'
+#' @return A \code{character(1)} representing the segment of code constructing
+#' \code{vec}
+#'
+#' @author Nicholas J. Eagles
+#'
+#' @examples
+#'
+#' fruits = c('apple', 'banana', 'cherry')
+#' message(vector_as_code(fruits))
+#'
 vector_as_code <- function(vec) {
     return(sprintf('c("%s")', paste(vec, collapse = '", "')))
 }
