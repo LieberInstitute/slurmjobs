12f1504def487a7b73cc9182b8ff95c40fe6a5b5
Nick-Eagles <nick.eagles@libd.org>
2023-10-18 16:54:15 -0400

    Add basic unit tests for remaining utils.R functions

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -83,3 +83,29 @@ test_that(
         test_combinations(this_list)
     }
 )
+
+test_that(
+    "get_short_flags",
+    {
+        fruits = c('coconut', 'cherry', 'banana', 'berry')
+        expect_equal(get_short_flags(fruits), c('c', 'a', 'b', 'd'))
+
+        #   Should fail for length > 26
+        expect_error(
+            get_short_flags(c(letters, letters)),
+            "Can't handle more than 26 loops"
+        )
+    }
+)
+
+test_that(
+    "vector_as_code",
+    {
+        fruits = c('coconut', 'cherry', 'banana', 'berry')
+        initials = vector_as_code(fruits)
+        expect_equal(
+            vector_as_code(fruits),
+            'c("coconut", "cherry", "banana", "berry")'
+        )
+    }
+)
