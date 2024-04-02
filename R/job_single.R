b4579fc734f980ce65d6683db01ce0090ede372c
Nick-Eagles <nick.eagles@libd.org>
2024-04-02 16:23:02 -0400

    Use HOSTNAME env var instead of SLURMD_NODENAME. Both work in slurmjobs, but this matches conventions used elsewhere at JHPCE

--- R/job_single.R
+++ R/job_single.R
@@ -144,7 +144,7 @@ echo "**** JHPCE info ****"
 echo "User: ${{USER}}"
 echo "Job id: ${{SLURM_JOB_ID}}"
 echo "Job name: ${{SLURM_JOB_NAME}}"
-echo "Node name: ${{SLURMD_NODENAME}}"
+echo "Node name: ${{HOSTNAME}}"
 echo "Task id: ${{SLURM_ARRAY_TASK_ID}}"
 
 ## Load the R module
