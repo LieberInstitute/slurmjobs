bf68763f9d88804a920d2411504f37aee50c499a
Nick-Eagles <nick.eagles@libd.org>
2025-02-03 11:43:24 -0500

    Add a parameter 'create_script' to job_loop() to allow independent creation of shell and R scripts. Part of #4

--- R/job_loop.R
+++ R/job_loop.R
@@ -6,6 +6,8 @@
 #' @param loops A named `list` where each of the elements are character vectors.
 #' The names of `loops` specify the variables used for the loops and the
 #' contents specify the options to loop through for each variable.
+#' @param create_script A `logical(1)` indicating whether to create the R
+#' script.
 #' @inheritParams job_single
 #'
 #' @return A length-2 list of character vectors containing the contents of the
@@ -21,12 +23,12 @@
 #'
 #' job_loop(
 #'     loops = list(region = c("DLPFC", "HIPPO"), feature = c("gene", "exon", "tx", "jxn")),
-#'     name = "bsp2_test_array", create_logdir = FALSE
+#'     name = "bsp2_test_array", create_logdir = FALSE,
 #'     cores = 2
 #' )
 #'
 job_loop <- function(loops, name, create_shell = FALSE, partition = "shared", memory = "10G",
-    cores = 1L, tc = 20, email = "ALL", logdir = "logs", create_logdir = TRUE) {
+    cores = 1L, tc = 20, email = "ALL", logdir = "logs", create_logdir = TRUE, create_script = create_shell) {
     ## Check that the loops are correctly defined
     if (!is.list(loops)) {
         stop("'loops' should be a named list.", call. = FALSE)
@@ -165,8 +167,8 @@ job_loop <- function(loops, name, create_shell = FALSE, partition = "shared", me
         script_core[version_line:length(script_core)]
     )
 
-    ## Write to a file?
-    if (create_shell) {
+    ## Write each component to file?
+    if (create_shell && create_script) {
         message(
             sprintf(
                 "%s Creating the shell file %s.sh and corresponding R script %s.R",
@@ -176,6 +178,26 @@ job_loop <- function(loops, name, create_shell = FALSE, partition = "shared", me
         message(sprintf("To submit the script pair, use: sbatch %s", sh_file))
         writeLines(r_text, con = sub("\\.sh$", ".R", sh_file))
         writeLines(script_final, con = sh_file)
+    } else {
+        if (create_shell) {
+            message(
+                sprintf(
+                    "%s Creating the shell file %s.sh",
+                    Sys.time(), name
+                )
+            )
+            message(sprintf("To submit the script pair, use: sbatch %s", sh_file))
+            writeLines(script_final, con = sh_file)
+        }
+        if (create_script) {
+            message(
+                sprintf(
+                    "%s Creating the R script %s.R",
+                    Sys.time(), name
+                )
+            )
+            writeLines(r_text, con = sub("\\.sh$", ".R", sh_file))
+        }
     }
 
     ## Done!
