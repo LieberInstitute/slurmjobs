41237408bcf4a9bd28c8da86498a88e2063403e8
Nick-Eagles <nick.eagles@libd.org>
2026-04-08 15:20:25 -0400

    Overhaul renumber() with some important safety checks; a renaming plan that implicitly involves deleting files or directories throws an error, and does so before touching any files on disk (unlike direct calls to file.rename())

--- R/renumber.R
+++ R/renumber.R
@@ -74,74 +74,50 @@ renumber <- function(
         )
     }
 
-    all_log_dirs = c()
-    for (i in seq_len(length(pre_before))) {
-        temp = .renumber_process_file(
-            base_dir = base_dir,
-            file_regex = "\\.(R|py)",
-            this_pre_before = pre_before[i],
-            this_pre_after = pre_after[i],
-            all_files = all_files,
-            is_shell = FALSE,
-            edit_content = TRUE,
-            expected_one_file = TRUE
-        )
-
-        temp = .renumber_process_file(
-            base_dir = base_dir,
-            file_regex = "\\.sh",
-            this_pre_before = pre_before[i],
-            this_pre_after = pre_after[i],
-            all_files = all_files,
-            is_shell = TRUE,
-            edit_content = TRUE,
-            expected_one_file = TRUE
-        )
-        all_log_dirs = c(all_log_dirs, temp)
-
-        temp = .renumber_process_file(
-            base_dir = base_dir,
-            file_regex = "(\\.(R|py|sh)|temp_slurmjobs)",
-            this_pre_before = pre_before[i],
-            this_pre_after = pre_after[i],
-            all_files = all_files,
-            is_shell = FALSE,
-            edit_content = FALSE,
-            expected_one_file = FALSE,
-            negate_pattern = TRUE
-        )
-    }
-
-    #   Remove temporary suffix from (non-log) file names
-    all_files <- list.files(base_dir, full.names = TRUE)
-    files_before <- all_files[
-        grepl(
-            sprintf(
-                "^(%s).*temp_slurmjobs$", paste(pre_after, collapse = "|")
-            ),
-            basename(all_files)
-        )
-    ]
-    files_after <- sub("temp_slurmjobs$", "", files_before)
-    file.rename(files_before, files_after)
-
-    #   Remove temporary suffix from log file names. This is slightly imprecise,
-    #   as in theory there can be a mismatch of prefix and log_dir, such that
-    #   a log file with the wrong prefix, also ending in 'temp_slurmjobs', could
-    #   be renamed. Ignore this highly unlikely case for now
-    for (log_dir in all_log_dirs) {
-        all_files <- list.files(log_dir, full.names = TRUE)
-        files_before <- all_files[
-            grepl(
-                sprintf(
-                    "^(%s).*temp_slurmjobs$", paste(pre_after, collapse = "|")
-                ),
-                basename(all_files)
+    #   Gather a full renaming plan of all files (source and destination paths),
+    #   without modifying any files
+    rename_plan_df = .renumber_loop(
+        base_dir, pre_before, pre_after, all_files, dry_run = TRUE
+    )
+  
+    #   Now simulate the plan to make sure it's safe. This could be done more
+    #   cleanly with dplyr::replace_values() but I don't want to depend on such
+    #   a recent version of dplyr
+    ending_paths = all_files
+    lookup = setNames(rename_plan_df$after, rename_plan_df$before)
+    ending_paths = case_when(
+            ending_paths %in% names(lookup) ~ lookup[ending_paths],
+            TRUE ~ ending_paths
+        ) |>
+        unname()
+    ending_paths = file.path(
+        base_dir, sub('temp_slurmjobs$', '', basename(ending_paths))
+    )
+    if (length(unique(ending_paths)) != length(all_files)) {
+        if (plots_and_processed) {
+            stop(
+                "The proposed renaming plan would effectively result in deletion of at least one file or directory. Please check which files exist in 'base_dir', corresponding plots and processed-data directories, and if supplied prefixes would result in an overwrite."
             )
-        ]
-        files_after <- sub("temp_slurmjobs$", "", files_before)
-        file.rename(files_before, files_after)
+        } else {
+            stop(
+                "The proposed renaming plan would effectively result in deletion of at least one file or directory. Please check which files exist in 'base_dir' and if supplied prefixes would result in an overwrite."
+            )
+        }
     }
+    
+    #   Now make the edits to the code files. Return the same plan as earlier
+    rename_plan_df = .renumber_loop(
+        base_dir, pre_before, pre_after, all_files, dry_run = FALSE
+    )
+
+    source_paths = rename_plan_df$before
+    intermediate_paths = rename_plan_df$after
+    destination_paths = file.path(
+        dirname(intermediate_paths),
+        sub('temp_slurmjobs$', '', basename(intermediate_paths))
+    )
+    file.rename(source_paths, intermediate_paths)
+    file.rename(intermediate_paths, destination_paths)
 
     #   For code directories, also renumber the corresponding 'processed-data'
     #   and 'plots' directories, if they exist
@@ -220,7 +196,7 @@ renumber <- function(
     }
 
     if (length(file_before) == 0) {
-        return(invisible(NULL))
+        return(data.frame(before = character(0), after = character(0)))
     }
 
     full_pre_before <- stringr::str_extract(
@@ -231,50 +207,56 @@ renumber <- function(
         paste0("^", this_pre_before), this_pre_after, full_pre_before
     )
     
-    log_dir = c()
-    if (edit_content) {
+    if (edit_content || is_shell) {
         file_content = readLines(file_before)
+    }
 
-        if (is_shell) {
-            #   For 'job_loop', use the path in the line starting with
-            #   'log_path='. Otherwise use the log in the line starting with
-            #   '#SBATCH -o'.
-            last_occurence <- rev(
-                grep("^(#SBATCH -o |log_path=)", file_content)
-            )[1]
-            log_dir <- file_content[last_occurence] |>
-                #   Extract just the path
-                str_replace("^(#SBATCH -o |log_path=)", "") |>
-                dirname()
+    log_files_before <- character(0)
+    log_files_after <- character(0)
+  
+    if (is_shell) {
+        #   For 'job_loop', use the path in the line starting with
+        #   'log_path='. Otherwise use the log in the line starting with
+        #   '#SBATCH -o'.
+        last_occurence <- rev(
+            grep("^(#SBATCH -o |log_path=)", file_content)
+        )[1]
+        log_dir <- file_content[last_occurence] |>
+            #   Extract just the path
+            str_replace("^(#SBATCH -o |log_path=)", "") |>
+            dirname()
 
-            #   If the log is specified with a relative path, make sure it's
-            #   relative to the directory containing the shell script
-            if (!fs::is_absolute_path(log_dir)) {
-                log_dir <- file.path(dirname(file_before), log_dir) |>
-                    normalizePath()
-            }
-
-            #   Use recursion to rename logs, where they exist
-            .renumber_process_file(
-                base_dir = log_dir,
-                file_regex = "\\.(txt|log)",
-                this_pre_before = this_pre_before,
-                this_pre_after = this_pre_after,
-                all_files = list.files(log_dir, full.names = TRUE),
-                is_shell = FALSE,
-                edit_content = FALSE,
-                expected_one_file = FALSE
-            )
+        #   If the log is specified with a relative path, make sure it's
+        #   relative to the directory containing the shell script
+        if (!fs::is_absolute_path(log_dir)) {
+            log_dir <- file.path(dirname(file_before), log_dir) |>
+                normalizePath(mustWork = FALSE)
         }
 
+        #   Use recursion to plan to rename logs, where they exist
+        rename_df = .renumber_process_file(
+            base_dir = log_dir,
+            file_regex = "\\.(txt|log)",
+            this_pre_before = this_pre_before,
+            this_pre_after = this_pre_after,
+            all_files = list.files(log_dir, full.names = TRUE),
+            is_shell = FALSE,
+            edit_content = FALSE,
+            expected_one_file = FALSE
+        )
+        log_files_before <- rename_df$before
+        log_files_after <- rename_df$after
+    }
+
+    if (edit_content) {
         #   Re-write the file in place, replacing references to the
         #   old prefix
         file_content <- gsub(full_pre_before, full_pre_after, file_content)
         writeLines(file_content, con = file_before)
     }
 
-    #   Rename scripts but append temporary suffix to avoid repeated
-    #   renaming
+    #   Plan to rename scripts but append temporary suffix to avoid repeated
+    #   renaming, since file.rename() executes renames sequentially
     file_after <- file.path(
         base_dir,
         paste0(
@@ -285,7 +267,58 @@ renumber <- function(
             "temp_slurmjobs"
         )
     )
-    file.rename(file_before, file_after)
+    plan_df = data.frame(
+        before = c(file_before, log_files_before),
+        after = c(file_after, log_files_after)
+    )
 
-    return(log_dir)
+    return(plan_df)
+}
+
+#' Helper function to gather a plan for renaming all files
+#' 
+#' This is employed by `renumber()`. A data.frame is returned with columns
+#' "before" and "after", indicating the full set of file paths to rename
+#' 
+#' @author Nicholas J. Eagles
+#' @keywords internal
+.renumber_loop = function(base_dir, pre_before, pre_after, all_files, dry_run) {
+    rename_plan_df_list = list()
+    for (i in seq_len(length(pre_before))) {
+        rename_plan_df_list[[length(rename_plan_df_list) + 1]] = .renumber_process_file(
+            base_dir = base_dir,
+            file_regex = "\\.(R|py)",
+            this_pre_before = pre_before[i],
+            this_pre_after = pre_after[i],
+            all_files = all_files,
+            is_shell = FALSE,
+            edit_content = !dry_run,
+            expected_one_file = TRUE
+        )
+
+        rename_plan_df_list[[length(rename_plan_df_list) + 1]] = .renumber_process_file(
+            base_dir = base_dir,
+            file_regex = "\\.sh",
+            this_pre_before = pre_before[i],
+            this_pre_after = pre_after[i],
+            all_files = all_files,
+            is_shell = TRUE,
+            edit_content = !dry_run,
+            expected_one_file = TRUE
+        )
+
+        rename_plan_df_list[[length(rename_plan_df_list) + 1]] = .renumber_process_file(
+            base_dir = base_dir,
+            file_regex = "\\.(R|py|sh)",
+            this_pre_before = pre_before[i],
+            this_pre_after = pre_after[i],
+            all_files = all_files,
+            is_shell = FALSE,
+            edit_content = FALSE,
+            expected_one_file = FALSE,
+            negate_pattern = TRUE
+        )
+    }
+    rename_plan_df = do.call(rbind, rename_plan_df_list)
+    return(rename_plan_df)
 }
